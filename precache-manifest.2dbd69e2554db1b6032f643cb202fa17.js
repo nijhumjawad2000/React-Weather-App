590b8d80b977eda51a2b0ada7152073912aa7415
nijhum_jawad2000 <njawad@techlaunch.io>
2019-03-12 18:14:57 -0400

    first commit

---
+++ precache-manifest.2dbd69e2554db1b6032f643cb202fa17.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "ae7c181cae193bd3235fb442319e6b20",
+    "url": "/React-Weather-App/static/media/pic4.ae7c181c.jpg"
+  },
+  {
+    "revision": "73649db39fa5d86660bf",
+    "url": "/React-Weather-App/static/js/runtime~main.b230b676.js"
+  },
+  {
+    "revision": "570e7b7512c5fbf8bed6",
+    "url": "/React-Weather-App/static/js/main.6f8ace45.chunk.js"
+  },
+  {
+    "revision": "93eb93d7ab342360b270",
+    "url": "/React-Weather-App/static/js/2.92e6ffcd.chunk.js"
+  },
+  {
+    "revision": "570e7b7512c5fbf8bed6",
+    "url": "/React-Weather-App/static/css/main.dbda4e51.chunk.css"
+  },
+  {
+    "revision": "b3a1d3f09279c3fb5c0f6b642ce755f7",
+    "url": "/React-Weather-App/index.html"
+  }
+];
